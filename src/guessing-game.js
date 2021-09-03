41879c0947341309ea1d75665f5ee8ae9ceb8bbe
YaryhaAD <YaryhaAD@gmail.com>
2021-09-03 16:22:31 +0300

    feat: implement class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,23 @@
+
 class GuessingGame {
-    constructor() {}
+    constructor() { }
 
     setRange(min, max) {
-
+        this.max = max;
+        this.min = min;
     }
 
     guess() {
-
+        this.try = Math.ceil((this.max + this.min) / 2);
+        return this.try;
     }
 
     lower() {
-
+        this.max = this.try;
     }
 
     greater() {
-
+        this.min = this.try;
     }
 }
 
